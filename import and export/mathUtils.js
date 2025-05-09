7699510cd1f6e170ebff0a37421fa89c5822fc0a
Kamal Doharey <kamal174590@gmail.com>
2025-05-09 16:03:13 +0530

    format with prettier

--- import and export/mathUtils.js
+++ import and export/mathUtils.js
@@ -1,19 +1,17 @@
-
 var myAdd = (a, b) => {
-    return a + b;
+	return a + b;
 };
 
 var mySub = (a, b) => {
-    return a - b;
+	return a - b;
 };
 
 var myMul = (a, b) => {
-    return a * b;
+	return a * b;
 };
 
 var myDiv = (a, b) => {
-    return a / b;
+	return a / b;
 };
 
-
-export { myAdd, mySub, myMul, myDiv }
+export { myAdd, mySub, myMul, myDiv };
